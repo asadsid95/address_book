86439c54f2378da8be37896b302294de409afa9d
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-15 00:08:17 -0500

    updating records via using ID provided for delete, to show current record matching ID -- #1

--- database.py
+++ database.py
@@ -98,16 +98,73 @@ def delete():
     conn.commit()
     conn.close
 
+def edit():
+    editor = Tk()
+    editor.title("Editor")
+
+    conn = sqlite3.connect('address_book.db')
+    cur = conn.cursor()
+
+    cur.execute("UPDATE address  *, oid FROM addresses")
+    profiles = cur.fetchall()
+    # print(profiles[0])
+
+    print_profiles = ''
+    for profile in profiles:
+        print_profiles += str(profile[0]) + " " + str(profile[1]) + "\t" + str(profile[6]) + "\n"
+
+    query_label = Label(root, text=print_profiles)
+    query_label.grid(row=11,column=0, columnspan=2)
+
+    conn.commit()
+    conn.close
+
+    # label fields
+    f_name_editor = Label(editor, text="First name")
+    f_name_editor.grid(row=0, column=0)
+    l_name_editor = Label(editor, text="Last name")
+    l_name_editor.grid(row=1, column=0)
+    address_editor = Label(editor, text="Address")
+    address_editor.grid(row=2, column=0)
+    city_editor = Label(editor, text="City")
+    city_editor.grid(row=3, column=0)
+    state_editor = Label(editor, text="State")
+    state_editor.grid(row=4, column=0)
+    zipcode_editor = Label(editor, text="Zipcode")
+    zipcode_editor.grid(row=5, column=0)
+
+    # user input
+    f_name =  Entry (editor, width=25)
+    f_name.grid(row=0, column=1)
+    l_name = Entry(editor, width=25)
+    l_name.grid(row=1, column=1)
+    address = Entry(editor, width=25)
+    address.grid(row=2, column=1)
+    city = Entry(editor, width=25)
+    city.grid(row=3, column=1)
+    state = Entry(editor, width=25)
+    state.grid(row=4, column=1)
+    zipcode = Entry(editor, width=25)
+    zipcode.grid(row=5, column=1)
+
+    update_button = Button(editor, text="Update Record")
+    update_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=120)
+
 # submit button
 submit_button=Button(root, text="Submit", command=submit)
-submit_button.grid(row=6,column=0,columnspan=2,pady=10,padx=10)
+submit_button.grid(row=6,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 # query button
 query_button=Button(root, text="Query", command=query)
-query_button.grid(row=7,column=0,columnspan=2,pady=10,padx=10)
+query_button.grid(row=7,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 # delete button
 delete_button=Button(root,text="Delete Records",command=delete)
-delete_button.grid(row=10,column=0,columnspan=2,pady=10,padx=10)
+delete_button.grid(row=10,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
+
+# note: create new window for editor
+edit_button=Button(root,text="Update Records",command=edit)
+edit_button.grid(row=10,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
+
 
 root.mainloop()
