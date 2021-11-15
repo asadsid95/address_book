87a2924b9113c8cc9856b2d4508295a99c1675c8
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-11-15 00:59:24 -0500

    updating functionality added -- #1

--- database.py
+++ database.py
@@ -26,7 +26,7 @@ state_label = Label(root, text="State")
 state_label.grid(row=4, column=0)
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
-delete_label = Label(root, text="Delete ID")
+delete_label = Label(root, text="Select ID")
 delete_label.grid(row=9,column=0)
 
 # user input
@@ -98,26 +98,55 @@ def delete():
     conn.commit()
     conn.close
 
+def update():
+    conn = sqlite3.connect('address_book.db')
+    cur = conn.cursor()
+
+    record_id = delete_entry.get()
+    cur.execute("""UPDATE addresses SET
+        first_name = :f_name,
+        last_name = :l_name,
+        address = :address,
+        city = :city,
+        state = :state,
+        zipcode = :zipcode
+
+        WHERE oid = :oid""",
+    {
+        'f_name': f_name.get(),
+        'l_name':l_name.get(),
+        'address':address.get(),
+        'city':city.get(),
+        'state':state.get(),
+        'zipcode':zipcode.get(),
+        'oid': record_id
+    }
+    )
+
+    conn.commit()
+    conn.close
+
+    editor.destroy()
+
 def edit():
+    global editor
     editor = Tk()
     editor.title("Editor")
 
     conn = sqlite3.connect('address_book.db')
     cur = conn.cursor()
 
-    cur.execute("UPDATE address  *, oid FROM addresses")
-    profiles = cur.fetchall()
-    # print(profiles[0])
+    record_id = delete_entry.get()
+    cur.execute("SELECT * FROM addresses WHERE oid=" + record_id)
+    records = cur.fetchall()
 
-    print_profiles = ''
-    for profile in profiles:
-        print_profiles += str(profile[0]) + " " + str(profile[1]) + "\t" + str(profile[6]) + "\n"
-
-    query_label = Label(root, text=print_profiles)
-    query_label.grid(row=11,column=0, columnspan=2)
-
-    conn.commit()
-    conn.close
+    # In order to use another function (i.e. update)
+    global f_name
+    global l_name
+    global address
+    global city
+    global state
+    global zipcode
 
     # label fields
     f_name_editor = Label(editor, text="First name")
@@ -147,23 +176,34 @@ def edit():
     zipcode = Entry(editor, width=25)
     zipcode.grid(row=5, column=1)
 
-    update_button = Button(editor, text="Update Record")
+    for record in records:
+        f_name.insert(0, record[0])
+        l_name.insert(0, record[1])
+        address.insert(0, record[2])
+        city.insert(0, record[3])
+        state.insert(0, record[4])
+        zipcode.insert(0, record[5])
+
+    update_button = Button(editor, text="Update Record", command=update)
     update_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=120)
 
+    conn.commit()
+    conn.close
+
 # submit button
-submit_button=Button(root, text="Submit", command=submit)
+submit_button=Button(root, text="Submit Record", command=submit)
 submit_button.grid(row=6,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 # query button
-query_button=Button(root, text="Query", command=query)
+query_button=Button(root, text="Query Records", command=query)
 query_button.grid(row=7,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 # delete button
-delete_button=Button(root,text="Delete Records",command=delete)
+delete_button=Button(root,text="Select Record",command=delete)
 delete_button.grid(row=10,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 # note: create new window for editor
-edit_button=Button(root,text="Update Records",command=edit)
+edit_button=Button(root,text="Update Record",command=edit)
 edit_button.grid(row=10,column=0,columnspan=2,pady=10,padx=10,ipadx=120)
 
 
